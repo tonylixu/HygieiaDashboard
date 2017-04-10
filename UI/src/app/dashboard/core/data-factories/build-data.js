dd34ffbda52e8cf3c16d1ebf8369eb80744c877a
tonylixu@gmail.com <tonylixu@gmail.com>
2017-04-10 14:01:10 -0400

    Added build-data.js file

---
+++ UI/src/app/dashboard/core/data-factories/build-data.js
@@ -0,0 +1,36 @@
+/**
+ * Gets build related data
+ */
+ (function () {
+    'use strict';
+
+    angular
+        .module(HygieiaConfig.module + '.core')
+        .factory('buildData', buildData);
+
+    function buildData($http) {
+        var testDetailRoute = 'test-data/build_detail.json';
+        var runDetailRoute = '/api/buildRun/';
+        var buildDetailRoute = '/api/build/';
+        var param = '?viewName=';
+
+        return {
+            details: details,
+            detailRun:detailRun
+        };
+
+        // Search for current builds
+        function details(params){
+            return $http.get(HygieiaConfig.local ? testDetailRoute : buildDetailRoute, { params: params })
+            .then(function (response)) {
+                return response.data;
+            });
+        }
+
+        function detailRun(viewName) {
+            return $http.get(runDetailRoute+ param + viewName).then(function (response) {
+                return response.data;
+            });
+        }
+    }
+ })();
